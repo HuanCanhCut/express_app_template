87fabad6265618e2015b4ffb82d49b0fa189e084
HuanCanhCut <tronghuanxxx@gmail.com>
2025-11-13 15:59:43 +0000

    refactor: add nodemon, remove auth controller methods and auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,12 +3,6 @@ const router = express.Router()
 
 import AuthController from '../app/controllers/AuthController'
 
-router.post('/register', AuthController.register.bind(AuthController))
-router.post('/login', AuthController.login.bind(AuthController))
-router.post('/logout', AuthController.logout.bind(AuthController))
-router.post('/loginwithtoken', AuthController.loginWithToken.bind(AuthController))
-router.get('/refresh', AuthController.refreshToken.bind(AuthController))
-router.post('/verify', AuthController.sendVerifyCode.bind(AuthController))
-router.post('/reset-password', AuthController.resetPassword.bind(AuthController))
+// router.post('/register', AuthController.register.bind(AuthController))
 
 export default router
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Express } from 'express'
 import authRoute from './auth'
 
 const route = (app: Express) => {
-    app.use('/api/auth', authRoute)
+    // app.use('/api/auth', authRoute)
 }
 
 export default route
